b66f827145566b0b12bfd35d3300b88e3ba2b6c8
wessypoo <wiselythewizard@gmail.com>
2026-02-12 15:09:23 -0800

    mangocommit1

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  const yourName = "Your Name Here";
+
+  const currentYear = 2026;
+
+
+  return (
+
+    <div>
+
+      <h1>Hello, React!</h1>
+
+      <h2>My name is {yourName}</h2>
+
+      <p>I’m learning React in {currentYear}.</p>
+
+      <p>This is my first React component!</p>
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
