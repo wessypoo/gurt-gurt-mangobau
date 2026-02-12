b5e4b6489297e1503bf454267e7c3899f8a6e58e
wessypoo <wiselythewizard@gmail.com>
2026-02-12 15:16:41 -0800

    mkkkk

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 
 function App() {
 
-  const yourName = "Your Name Here";
+  const yourName = "hi";
 
   const currentYear = 2026;
 
+  const colorlel = "blue";
+
+  const imageUrl = "https://www.pngall.com/wp-content/uploads/5/React-Logo-PNG-Picture.png";
 
   return (
 
@@ -18,6 +21,16 @@ function App() {
       <p>I’m learning React in {currentYear}.</p>
 
       <p>This is my first React component!</p>
+
+      <ul>
+        <li> I wanna build some surveys and forms i guess</li>
+        <li> I wanna makea mango or something</li>
+        <li> I wanna make a website for my portfolio</li>
+      </ul>
+
+      <p>{colorlel}</p>
+
+      <p>{imageUrl}</p>
 
     </div>
 
